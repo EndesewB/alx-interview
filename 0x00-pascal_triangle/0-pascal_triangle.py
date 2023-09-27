6d4d0d9e20a67dab19c57d30e99d3492ee22282b
Endesew Bantayehu <117736159+EndesewB@users.noreply.github.com>
2023-09-27 20:27:20 +0300

    -pascal_triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,30 @@
+def pascal_triangle(n):
+    """
+    Generate Pascal's triangle up to the nth row.
+
+    Args:
+        n (int): The number of rows to generate.
+
+    Returns:
+        list of list of int: A list of lists representing Pascal's triangle.
+                            Each inner list represents a row in the triangle.
+                            Returns an empty list if n <= 0.
+    """
+    if n <= 0:
+        return []
+
+    triangle = []
+
+    for i in range(n):
+        if i == 0:
+            row = [1]
+        else:
+            prev_row = triangle[-1]
+            row = [1]
+            for j in range(1, i):
+                row.append(prev_row[j - 1] + prev_row[j])
+            row.append(1)
+
+        triangle.append(row)
+
+    return triangle
