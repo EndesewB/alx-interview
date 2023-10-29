5d298084e3749709e18e65c740a280b747686b8b
Endesew Bantayehu <117736159+EndesewB@users.noreply.github.com>
2023-10-29 10:19:44 +0300

    Update 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -8,20 +8,20 @@ def validUTF8(data):
     """
     valid_leading_bytes = 0
     for byte in data:
-        if byte >> 6 == 0b10:
-            valid_leading_bytes -= 1
-            if valid_leading_bytes < 0:
+        if byte >= 128 and byte <= 191:
+            if valid_leading_bytes <= 0:
                 return False
+            valid_leading_bytes -= 1
         else:
             if valid_leading_bytes > 0:
                 return False
-            if byte >> 7 == 0:
+            if byte < 128:
                 valid_leading_bytes = 0
-            elif byte >> 5 == 0b110:
+            elif byte >= 192 and byte <= 223:
                 valid_leading_bytes = 1
-            elif byte >> 4 == 0b1110:
+            elif byte >= 224 and byte <= 239:
                 valid_leading_bytes = 2
-            elif byte >> 3 == 0b11110:
+            elif byte >= 240 and byte <= 247:
                 valid_leading_bytes = 3
             else:
                 return False
