6437ab99296f24dfe1f5cca7863752a4ec9419f4
Endesew Bantayehu <117736159+EndesewB@users.noreply.github.com>
2023-11-15 15:14:21 +0300

    Update 0-rotate_2d_matrix.py

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -31,5 +31,29 @@ def rotate_2d_matrix(matrix):
                     matrix (_type_): _description_
     """
     n = len(matrix)
-    
+    # print(n)
+
+    """sample matrix
+    1 2 3
+    4 5 6
+    7 8 9
+    """
+
+    # transpose matrix
+    """
+    1 4 7
+    2 5 8
+    3 6 9
+    """
+
+    transpose_matrix(matrix, n)
+
+    # reverse matrix
+    """
+    7 4 1
+    8 5 2
+    9 6 3
+    """
+    reverse_matrix(matrix)
+
     return matrix
